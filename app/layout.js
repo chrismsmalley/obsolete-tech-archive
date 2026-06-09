e26eceada316e321c04e7e560e3ba331eeefb477
Chris Smalley <christophermsmalley@gmail.com>
2026-06-09 12:29:12 -0400

    adsense updates

--- app/layout.js
+++ app/layout.js
@@ -50,6 +50,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-1718924103860171"
+        />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1718924103860171"
